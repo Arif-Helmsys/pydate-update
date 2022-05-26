98c46f5a36383939b217e21948f91dd2625dc54a
Arif <84701901+Arif-Helmsys@users.noreply.github.com>
2022-05-26 20:07:02 +0300

    Update PyDateErrors.py

--- Pydate/Exceptions/PyDateErrors.py
+++ Pydate/Exceptions/PyDateErrors.py
@@ -1,8 +1,14 @@
 class PathIsEmpty(Exception):
-    " Yol Belirtilmediyse Hata Verir "
+    " Error If Path Is Not Specified "
     def __init__(self,*args: object) -> None:
         super().__init__("Dosya Yolu Bulunamadı")
 
 class LogicError(Exception):
+    " Logic throws an error when a mistake is made "
     def __init__(self, *args: object) -> None:
         super().__init__("Mantıksal bir hata")
+
+class VersionFileNotFound(Exception):
+    " Error when version file does not exist "
+    def __init__(self, *args: object) -> None:
+        super().__init__("Create version.json first!")
